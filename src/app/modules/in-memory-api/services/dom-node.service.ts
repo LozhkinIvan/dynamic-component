e66578dbae26a5ef5f738e915b3608ef8d1b7be4
Lozhkin Ivan <lozhkin@proitr.ru>
2017-06-13 23:17:24 +0500

    Fix compilation error

--- src/app/modules/in-memory-api/services/dom-node.service.ts
+++ src/app/modules/in-memory-api/services/dom-node.service.ts
@@ -1,4 +1,4 @@
-import { InMemoryDbService } from 'angular2-in-memory-web-api';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
 import { IDomNode } from 'app/modules/app/interfaces/idom-node';
 
 /**
